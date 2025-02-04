13b851a56adabb7f37dd18f99243149a51940596
adv <jxradv@gmail.com>
2025-02-04 19:48:50 +0500

    first

--- configs/settings.py
+++ configs/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vgq3di*buldgiv#7f)t$^9lffymg9mdw(y6ze&rj!h%sm+dh=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
